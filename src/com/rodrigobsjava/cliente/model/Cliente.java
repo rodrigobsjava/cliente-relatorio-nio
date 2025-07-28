51075615af16af3815daf7aa43316c118bcf2e35
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-07-28 17:54:08 -0400

    cria projeto e classe Cliente no pacote model

---
+++ src/com/rodrigobsjava/cliente/model/Cliente.java
@@ -0,0 +1,19 @@
+package com.rodrigobsjava.cliente.model;
+
+public class Cliente {
+	private String nome;
+	private String email;
+	private int idade;
+
+	public Cliente(String nome, String email, int idade) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+	}
+
+	public String formatado() {
+		return String.format("Nome: %s | Email: %s | Idade: %d", nome, email, idade);
+	}
+
+}
